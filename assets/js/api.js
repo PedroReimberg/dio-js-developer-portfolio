e71f570acfc1af3ec8d27cf672ca7377b36a524a
PedroReimberg <pedrohreimberg@gmail.com>
2024-03-13 23:50:23 -0300

    feat(api): Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
-	const url =
-		'https://raw.githubusercontent.com/PedroReimberg/dio-js-developer-portfolio/master/data/profile.json'
+	const url = './data/profile.json'
+	// 'https://raw.githubusercontent.com/PedroReimberg/dio-js-developer-portfolio/master/data/profile.json'
 	const fetching = await fetch(url)
 	return await fetching.json()
 }
